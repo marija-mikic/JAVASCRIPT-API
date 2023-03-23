76a2af650042ae5228c2c26ba6704ebe161c457d
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2023-03-23 23:04:11 +0100

    changes in appearance and number of products  page

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
 async function fetchAPIData() {
   fetch("https://dummyjson.com/products")
     .then((res) => res.json())
-    .then(
-      (data) => (
-        console.log(data.products),
-        showProduct(data.products),
-        wiperSlider(data.products)
-      )
-    );
-
-  let itemPerPage = 10;
-  let curentPage = 1;
-
-  const pages = [];
-  for (let i = 0; i < Math.ceil(fetchAPIData.length / itemPerPage); i++) {
-    pages.push(i);
-  }
-  const indexOfLastPage = curentPage * itemPerPage;
-  const indexOfFirstPage = indexOfLastPage - itemPerPage;
-  const curentItem = pages.slice(indexOfFirstPage, indexOfLastPage);
+    .then((data) => (console.log(data.products), wiperSlider(data.products)));
 }
-
 const productId = window.location.search.split("=")[1];
 
 async function APIData() {
@@ -50,35 +32,13 @@ function init() {
 }
 document.addEventListener("DOMContentLoaded", init);
 
-async function showProduct(data) {
-  data.forEach((product) => {
-    const div = document.createElement("div");
-    div.classList.add("card");
-    div.innerHTML = `
-    <a href="product.html?id=${product.id}"><img src="${product.thumbnail}" alt="${product.brand}">
-          <div class="wiper-info">
-                <p2>${product.description}</p2>
-                 
-            </div>
-          </a>
-          <p>${product.category}</p>
-          <div class="card-body">
-            <h5 class="card-title">${product.brand}</h5>
-            
-          </div>
-        `;
-
-    document.querySelector("#popular-product").appendChild(div);
-  });
-}
-
 async function productDetail(product) {
   const productId = window.location.search.split("=")[1];
 
   const div = document.createElement("div");
   div.innerHTML = `<div class="details-top">
           <div>
-            <img src="${product.thumbnail}" />
+            <img src="${product.images[2]}" />
           </div>
           <div>
             <h2>${product.description}</h2>
@@ -90,7 +50,6 @@ async function productDetail(product) {
             <p>
               ${product.category}
             </p>
-
            
           </div>`;
   document.querySelector("#product-details").appendChild(div);
@@ -106,10 +65,9 @@ async function wiperSlider(data) {
 
     div.innerHTML = `
       <a href="product.html?id=${swiper.id}"">
-        <img src=" ${swiper.thumbnail}" alt="${swiper.title}" />
+        <img src=" ${swiper.images[0]}" alt="${swiper.title}" />
       </a>
-      <p>${swiper.description}</p>
-      <h3>${swiper.title}</h3>
+      <p>${swiper.title}</p>
        
   
     `;
@@ -142,3 +100,47 @@ function initSwiper() {
   });
 }
 initSwiper();
+
+var url = "https://dummyjson.com/products";
+var productData = [];
+var itemsPerPage = 12;
+var curentPage = 1;
+
+async function dataTable() {
+  await productTable();
+  const pages = [];
+  for (let i = 0; i < Math.ceil(productData.length / itemsPerPage); i++) {
+    pages.push(i);
+  }
+  const indexOfLastPage = curentPage * itemsPerPage;
+  const indexOfFirstPage = indexOfLastPage - itemsPerPage;
+  const curentItems = productData.slice(indexOfFirstPage, indexOfLastPage);
+  console.log(curentItems);
+  document.getElementById("popular-product").innerHTML = curentItems
+    .map(
+      (products) => `
+         <div class="card">
+    <a href="product.html?id=${products.id}"><img src="${products.images[2]}" alt="${products.brand}">
+          <div class="wiper-info">
+                <p2>${products.description}</p2>
+                 
+            </div>
+          </a>
+          <p>${products.category}</p>
+          <div class="card-body">
+            <h5 class="card-title">${products.brand}</h5>
+            </div>
+          </div>
+    `
+    )
+    .join("");
+}
+dataTable();
+
+async function productTable() {
+  const data = await fetch(url);
+  const res = await data.json();
+  productData = res.products;
+  console.log(productData);
+}
+productTable();
